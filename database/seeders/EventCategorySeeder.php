d7eccfc4bdc839a566446e5f57d887c4db58eb63
haykal24 <moh.haikal02@gmail.com>
2025-11-23 18:37:21 +0700

    p

--- database/seeders/EventCategorySeeder.php
+++ database/seeders/EventCategorySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\EventCategory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class EventCategorySeeder extends Seeder
 {
@@ -15,30 +16,35 @@ class EventCategorySeeder extends Seeder
     {
         EventCategory::create([
             'name' => 'Running',
+            'slug' => Str::slug('Running'),
             'description' => 'Marathon, Fun Run, Sprint',
             'icon' => 'run.png'
         ]);
 
         EventCategory::create([
             'name' => 'Swimming',
+            'slug' => Str::slug('Swimming'),
             'description' => 'Freestyle, Butterfly, Breaststroke',
             'icon' => 'swim.png'
         ]);
 
         EventCategory::create([
             'name' => 'Boxing',
+            'slug' => Str::slug('Boxing'),
             'description' => 'Amateur Boxing, Professional Boxing',
             'icon' => 'boxing.png'
         ]);
 
         EventCategory::create([
             'name' => 'Tennis',
+            'slug' => Str::slug('Tennis'),
             'description' => 'Singles, Doubles, Mixed Doubles',
             'icon' => 'tennis.png'
         ]);
 
         EventCategory::create([
             'name' => 'Powerlifting',
+            'slug' => Str::slug('Powerlifting'),
             'description' => 'Deadlift, Squat, Bench Press',
             'icon' => 'power.png'
         ]);
